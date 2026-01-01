50db0bc586f3d919af6233e348384f5c89d0cf3f
yumee2 <miyazaki1707@gmail.com>
2026-01-01 20:19:44 +0700

    hellotest finished

--- hello.go
+++ hello.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	russian = "Russian"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	russianHelloPrefix = "Привет, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,12 +4,17 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+		got := Hello("Крис", "Russian")
+		want := "Привет, Крис"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying bonjour to french people", func(t *testing.T) {
+		got := Hello("Chris", "French")
+		want := "Bonjour, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying default value when string is empty", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
