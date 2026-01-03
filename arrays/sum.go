8c1341da3c1421bfbe7f4e4010939f4cef90668e
yumee2 <miyazaki1707@gmail.com>
2026-01-03 21:13:04 +0700

    arrays test

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,3 +9,28 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbers ...[]int) []int {
+	sums := make([]int, len(numbers))
+
+	for index, val := range numbers {
+		sumOfArray := Sum(val)
+		sums[index] = sumOfArray
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
