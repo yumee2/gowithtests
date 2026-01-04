16ada9d9ad70146f11ac87f815617a5e9c821077
yumee2 <miyazaki1707@gmail.com>
2026-01-04 11:03:48 +0700

    struct test: table-driven test

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,6 +1,24 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs/struct_test.go
+++ structs/struct_test.go
@@ -13,10 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
 	}
 }
