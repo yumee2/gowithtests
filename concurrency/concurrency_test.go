d2ccafb3f33aa961c90a3d39f5a75abdddee2f49
yumee2 <miyazaki1707@gmail.com>
2026-01-08 12:34:49 +0700

    concurrenct test finished

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -1,9 +1,34 @@
 package concurrency
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
 
 func slowWebsiteChecker(_ string) bool {
 	time.Sleep(20 * time.Millisecond)
@@ -17,7 +42,7 @@ func BenchmarkCheckWebsites(b *testing.B) {
 		urls[i] = "a url"
 	}
 
-	for b.Loop() {
+	for i := 0; i < b.N; i++ {
 		CheckWebsites(slowWebsiteChecker, urls)
 	}
 }
