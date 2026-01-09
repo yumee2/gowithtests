6591fe13e2843f816cb405e2e1bfe06184e2db77
yumee2 <miyazaki1707@gmail.com>
2026-01-09 11:48:39 +0700

    arrays & maps

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -10,9 +10,13 @@ func walk(x interface{}, fn func(input string)) {
 		for i := 0; i < val.NumField(); i++ {
 			walk(val.Field(i).Interface(), fn)
 		}
-	case reflect.Slice:
+	case reflect.Slice, reflect.Array:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
 		}
 	case reflect.String:
 		fn(val.String())
